71d86df3eace34346397f2b824dff01f3c8bb276
CodeNinja-tech <elo48858@gmail.com>
2022-11-01 22:29:24 +0100

    `⚒️ fix: Fixed Page Query String Issue

--- server/src/controllers/anime/anime.controllers.ts
+++ server/src/controllers/anime/anime.controllers.ts
@@ -2,6 +2,42 @@ import axios from 'axios'
 import { Request, Response } from 'express'
 
 class AnimeController {
+  /**
+   * Get a specific anime based on it's title
+   * @param req
+   * @param res
+   */
+  public async get_specific(
+    search: string,
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      if (search) {
+        const result = await axios.get(
+          `https://api.consumet.org/anime/gogoanime/${search}`
+        )
+        res.status(200).json(result.data)
+      } else {
+        throw new Error('cannot find that anime')
+      }
+    } catch (err) {
+      res.once('error', () => {
+        res.status(401)
+      })
+      res.json({
+        err: 'Cannot find that anime at this time',
+      })
+      console.log(err)
+    }
+  }
+
+  /**
+   * Get the top airing anime episodes from a remote server
+   * @param page
+   * @param req
+   * @param res
+   */
   public async get_anime(page = 1, req: Request, res: Response): Promise<void> {
     try {
       const top_airing_anime = await axios.get(
